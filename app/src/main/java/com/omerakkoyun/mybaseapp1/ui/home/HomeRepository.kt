a99b94c952497298644d37e1c4ad158d6d9ebd80
omer akkoyun <omer_13_34@hotmail.com>
2023-04-25 15:59:14 +0300

    edit

--- app/src/main/java/com/omerakkoyun/mybaseapp1/ui/home/HomeRepository.kt
+++ app/src/main/java/com/omerakkoyun/mybaseapp1/ui/home/HomeRepository.kt
@@ -1,13 +1,18 @@
 package com.omerakkoyun.mybaseapp1.ui.home
 
 import com.omerakkoyun.mybaseapp1.base.BaseRepository
+import com.omerakkoyun.mybaseapp1.models.NetworkResult
+import com.omerakkoyun.mybaseapp1.models.cryptoResponse.CryptoResponse
 import com.omerakkoyun.mybaseapp1.network.ApiFactory
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(private val apiFactory: ApiFactory): BaseRepository(){
 
-    suspend fun getCoins(apiKey:String, limit:String) = safeApiRequest {
+    suspend fun getCoins(apiKey:String, limit:String): NetworkResult<CryptoResponse>{
+        return safeApiRequest {
             apiFactory.getCoins(apiKey,limit)
+        }
+        
     }
 
 }
